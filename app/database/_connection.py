f6b8f50fefed0e21faa89744733f43e0441402fb
Fred Nandrin <fred@nanit.be>
2024-11-04 16:41:00 +0100

    database in ENV

--- app/database/_connection.py
+++ app/database/_connection.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
+from decouple import config
 
-MONGO_DETAILS = "mongodb://root:password@localhost:27017"; # config('MONGO_DATABASE')
+MONGO_DETAILS =  "mongodb://" +config('MONGODB_ADMINUSERNAME')+":"+config('MONGODB_ADMINPASSWORD')+"@"+ config('MONGO_DATABASE') # "mongodb://root:password@localhost:27017"; 
 client = MongoClient(MONGO_DETAILS)
 
 database = client.myapi
