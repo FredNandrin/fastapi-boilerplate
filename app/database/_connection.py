c2088dbf5701a90c143c5909c4a3fbf5eeb3e80e
Fred Nandrin <fred@nanit.be>
2024-11-04 16:55:59 +0100

    -

--- app/database/_connection.py
+++ app/database/_connection.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from decouple import config
 
-MONGO_DETAILS =  "mongodb://" +config('MONGODB_ADMINUSERNAME')+":"+config('MONGODB_ADMINPASSWORD')+"@"+ config('MONGO_DATABASE') # "mongodb://root:password@localhost:27017"; 
+MONGO_DETAILS =  "mongodb://" +config('MONGODB_ADMINUSERNAME')+":"+config('MONGODB_ADMINPASSWORD')+"@"+ config('MONGO_DATABASE')+":"+config('MONGO_DATABASE_PORT') # "mongodb://root:password@localhost:27017"; 
 client = MongoClient(MONGO_DETAILS)
 
 database = client.myapi
